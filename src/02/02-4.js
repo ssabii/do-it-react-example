bd8c28dd60fd44b06330927b2e9bce7f5727aac9
ssabii <fhy1930@gmail.com>
2021-12-26 04:57:59 +0900

    02-3 가변 변수와 불변 변수

---
+++ src/02/02-4.js
@@ -0,0 +1,40 @@
+// let num = 1;
+// num = num * 3;
+// let str = '문자';
+// str = '다른 문자';
+// let arr = [];
+// arr = [1, 2, 3];
+// let obj = {};
+// obj = { name: '새 객체' };
+
+// const num = 1;
+// num = 3;
+// const str = '문자';
+// str = '새 문자';
+// const arr = [];
+// arr = [1, 2, 3];
+// const obj = {};
+// obj = { name: '내 이름' };
+
+// const arr2 = [];
+// arr2.push(1);
+// arr2.splice(0, 0, 0);
+// arr2.pop();
+// const obj2 = {};
+// obj2['name'] = '내 이름';
+// Object.assign(obj2, { name: '새이름' });
+// delete obj2.name;
+
+const num1 = 1;
+const num2 = num1 * 3;
+const str1 = '문자';
+const str2 = str1 + '추가';
+const arr3 = [];
+const arr4 = arr3.concat(1);
+const arr5 = [...arr4, 2, 3];
+const arr6 = arr5.slice(0, 1);
+const [first, ...arr7] = arr5;
+const obj3 = { name: '새이름', age: 20 };
+const objValue = { name: '새이름', age: obj3.age };
+const obj4 = { ...obj3, name: '새이름' };
+const { name, ...obj5 } = obj4;
