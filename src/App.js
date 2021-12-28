b8e07f9cf1d3c4de6b42540dace57742decb490e
ssabii <fhy1930@gmail.com>
2021-12-29 01:05:05 +0900

    03-2 컴포넌트와 구성 요소

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import TodaysPlan from './03/TodaysPlan';
 
 class App extends Component {
   render() {
     return (
       // 아래는 JSX 양식입니다.
-      <div>
-        <img src="http://www.easypub.co.kr/images/logo_footer.png" />
-        <div>안녕하세요</div>
+      <div className="body">
+        <TodaysPlan />
       </div>
     );
   }
