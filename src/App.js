16ecae92fbe5f152d555217c6c648be70f76eda8
ssabii <fhy1930@gmail.com>
2022-01-02 01:16:38 +0900

    03-5 컴포넌트의 생명주기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import ForceUpdateExample from './03/ForceUpdateExample';
+
+import Counter from './03/Counter';
+import NewCounter from './03/NewCounter';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 10 };
+    this.resetCount = this.resetCount.bind(this);
+  }
+
+  resetCount() {
+    this.setState(({ count }) => ({ count: count + 10 }));
+  }
+
+  componentDidMount() {
+    this.setState({ hasDestroyed: true });
+  }
+
   render() {
     return (
       <div>
-        <ForceUpdateExample />
+        <div>
+          <Counter count={this.state.count} />
+        </div>
+        <div>
+          <NewCounter count={this.state.count} />
+        </div>
+        <button onClick={this.resetCount}>{this.state.count + 10}으로 초기화</button>
       </div>
     );
   }
