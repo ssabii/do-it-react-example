c700d1f52e34759792b66c8213aec1f220ec9d56
ssabii <fhy1930@gmail.com>
2022-01-02 01:57:47 +0900

    03-8 배열 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
 
-import Counter from './03/Counter';
-import NewCounter from './03/NewCounter';
+import Todolist from './03/Todolist';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 10 };
-    this.resetCount = this.resetCount.bind(this);
-  }
-
-  resetCount() {
-    this.setState(({ count }) => ({ count: count + 10 }));
-  }
-
-  componentDidMount() {
-    this.setState({ hasDestroyed: true });
-  }
-
   render() {
     return (
       <div>
-        <div>
-          <Counter count={this.state.count} />
-        </div>
-        <div>
-          <NewCounter count={this.state.count} />
-        </div>
-        <button onClick={this.resetCount}>{this.state.count + 10}으로 초기화</button>
+        <Todolist />
       </div>
     );
   }
