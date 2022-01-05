ca9bd333d76687532097731c5aedb777a0b7a78e
ssabii <fhy1930@gmail.com>
2022-01-05 23:28:09 +0900

    04-2 CSS로 컴포넌트 스타일 적용하기 04-3 스타일 컴포넌트 만들기

---
+++ src/04/Button.jsx
@@ -0,0 +1,95 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import withStyles, { css } from './withStyles';
+
+class Button extends PureComponent {
+  render() {
+    const {
+      children,
+      disabled,
+      onPress,
+      styles,
+      large,
+      xlarge,
+      small,
+      xsmall,
+      primary,
+      secondary,
+    } = this.props;
+    return (
+      <button
+        {...css(
+          styles.default,
+          xsmall && styles.xsmall,
+          small && styles.small,
+          large && styles.large,
+          xlarge && styles.xlarge,
+          secondary && styles.secondary,
+          primary && styles.primary,
+        )}
+        onClick={onPress}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
+Button.protoTypes = {
+  children: PropTypes.node.isRequired,
+  onPress: PropTypes.func,
+  xsmall: PropTypes.bool,
+  small: PropTypes.bool,
+  large: PropTypes.bool,
+  xlarge: PropTypes.bool,
+  secondary: PropTypes.bool,
+  primary: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  onPress: () => {},
+  xsmall: false,
+  small: false,
+  large: false,
+  xlarge: false,
+  secondary: false,
+  primary: false,
+};
+
+export default withStyles(({ color, size, unit, responsive }) => ({
+  default: {
+    border: 1,
+    borderStyle: 'solid',
+    borderColor: color.default,
+    borderRadius: 2,
+    bcolor: color.default,
+    fontSize: size.md,
+    padding: unit * 2,
+    cursor: 'pointer',
+    [responsive.small]: {
+      width: '100%',
+    },
+  },
+  xlarge: {
+    fontSize: size.xg,
+  },
+  large: {
+    fontSize: size.lg,
+  },
+  small: {
+    fontSize: size.sm,
+  },
+  xsmall: {
+    fontSize: size.sm,
+    padding: unit,
+  },
+  primary: {
+    borderColor: color.primary,
+    color: color.white,
+    backgroundColor: color.primary,
+  },
+  secondary: {
+    borderColor: color.secondary,
+    color: color.secondary,
+  },
+}))(Button);
