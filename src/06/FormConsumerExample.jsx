3c819b5ba56d5d751a81b370403e12775e36429a
ssabii <fhy1930@gmail.com>
2022-02-03 22:40:37 +0900

    06-5 입력 폼 만들며 컨텍스트 복습하기

---
+++ src/06/FormConsumerExample.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Input from '../04/InputWithStyle';
+import { Consumer } from './FormContext';
+
+class FormConsumerExample extends Component {
+  render() {
+    const { name, ...otherProps } = this.props;
+    return (
+      <Consumer>
+        {({ values, errors, onChange }) => (
+          <Input
+            {...otherProps}
+            name={name}
+            onChange={onChange}
+            value={values[name]}
+            errorMessage={errors[name]}
+          />
+        )}
+      </Consumer>
+    );
+  }
+}
+
+export default FormConsumerExample;
