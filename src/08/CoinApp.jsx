ff0b84fa4a85887d168f9bb420a304f6b5bedc4f
ssabii <fhy1930@gmail.com>
2022-04-14 00:40:14 +0900

    12-2 리액트 라우터 구성하기

--- src/08/CoinApp.jsx
+++ src/08/CoinApp.jsx
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import AppLayout from './components/AppLayout';
 import MainPage from './components/main/MainPage';
 import ModalProvider from './ModalProvider';
 import NotificationContainer from './containers/NotificationContainer';
+import NotFound from './components/NotFound';
 
 class CoinApp extends PureComponent {
   store = configureStore();
@@ -13,12 +15,17 @@ class CoinApp extends PureComponent {
   render() {
     return (
       <Provider store={this.store}>
-        <ModalProvider>
-          <AppLayout>
-            <MainPage />
-            <NotificationContainer />
-          </AppLayout>
-        </ModalProvider>
+        <Router>
+          <ModalProvider>
+            <AppLayout>
+              <Switch>
+                <Route path="/" exact render={() => <MainPage />} />
+                <Route path="*" component={NotFound} />
+              </Switch>
+              <NotificationContainer />
+            </AppLayout>
+          </ModalProvider>
+        </Router>
       </Provider>
     );
   }
